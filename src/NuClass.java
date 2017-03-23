4efac8184bd40584d475aea27b86d0c6f2ee8758
swatich94 <swatich@bu.edu>
2017-03-23 01:18:56 -0400

    Added function quantity
    To calculate price of number of packages delivered

--- src/NuClass.java
+++ src/NuClass.java
@@ -23,7 +23,13 @@ public class NuClass {
 		else if(category.equals("electronics")){ctg_markup=((b_markup*2)/100);}
 		else{ctg_markup = 0.0;}
 		
-		double final_amount =  b_markup+person_markup+ctg_markup;
+		double amount =  b_markup+person_markup+ctg_markup;
+		return amount;
+	}
+	
+	static double quantity(double amount,int no_packages)
+	{
+		double final_amount= amount*no_packages;
 		return final_amount;
 	}
 	public static void main(String args[])
@@ -38,7 +44,12 @@ public class NuClass {
 	    price =c[0];
 	    person =c[1];
 	    category=c[2];
-		double final_price=mark_up(price,person,category);
+		Scanner inp = new Scanner(System.in);
+	    System.out.println("Enter number of packages to be delivered");
+	    int quant = inp.nextInt();
+	    double amt=mark_up(price,person,category);
+	    double final_price = quantity(amt,quant);
+	   
 		System.out.println(" "+final_price);
 	}
 
